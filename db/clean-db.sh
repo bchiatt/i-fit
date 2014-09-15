f3ac5960749a6553be6b0e8490466543abefdfd8
Brian Hiatt <brian.hiatt41@gmail.com>
2014-09-15 16:49:14 -0500

    user info received from db to profile view

--- db/clean-db.sh
+++ db/clean-db.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-    mongoimport --jsonArray --drop --db $1 --collection users --file ./users.json
-    mongoimport --jsonArray --drop --db $1 --collection food --file ./food.json
-    mongoimport --jsonArray --drop --db $1 --collection exercise --file ./exercise.json
+if [ -z "$1" ] ; then
+  echo "Enter a database name"
+  exit 1
+fi
 
-
+mongoimport --jsonArray --drop --db $1 --collection users --file ./users.json
+mongoimport --jsonArray --drop --db $1 --collection food --file ./food.json
+mongoimport --jsonArray --drop --db $1 --collection exercise --file ./exercise.json
