11668698b9bfa60fe6f53c4d343b81bfa835863f
matt keef <mkeef1@gmail.com>
2014-09-16 15:31:17 -0500

    form stuff

--- client/views/profile/profile.js
+++ client/views/profile/profile.js
@@ -4,6 +4,10 @@
   angular.module('i-fit')
   .controller('ProfileCtrl', ['$scope', 'User', function($scope, User){
 
+    User.show().then(function(response){
+      $scope.client = response.data.client;
+    });
+
     $scope.update = function(){
       User.update($scope.user).then(function(response){
         console.log(response);
