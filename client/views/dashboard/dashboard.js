1b2fdb77c19dff646657db22ec0dcdef7ee31377
matt keef <mkeef1@gmail.com>
2014-09-16 15:12:00 -0500

    user form

--- client/views/dashboard/dashboard.js
+++ client/views/dashboard/dashboard.js
@@ -3,6 +3,11 @@
 
   angular.module('i-fit')
   .controller('DashboardCtrl', ['$scope', function($scope){
+    $scope.foodType = ['Fruit', 'Vegetable', 'Carbohydrate', 'Protein', 'Dairy', 'Other'];
+
+    $scope.toggleFood = function(){
+      $scope.hideFood = !!!$scope.hideFood;
+    };
   }]);
 })();
 
